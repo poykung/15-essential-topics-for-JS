4ce8935fe0aff11e30529f10ce0a76cc6e910663
Giorno Giovanna <135321183+poykung@users.noreply.github.com>
2023-06-13 17:17:26 +0700

    Add files via upload

---
+++ array_properties-length-sort-push.js
@@ -0,0 +1,23 @@
+/*array properties
+length sort push
+*/
+
+
+
+let characters = ['A','B','C','D','F','Z','G','W','Y']
+
+//นับสมาชิกที่อยู่ภายใน array .length
+//console.log(characters.length)
+
+//เรียงลำดับสมาชิกที่อยู่ภายใน array sort()
+//console.log(characters.sort())
+
+//การเข้าถึงสมาชิกภายใน array ตัวแรกและตัวสุดท้าย
+//console.log(characters[0])
+//console.log(characters[characters.length-1])
+
+//เพิ่มสมาชิกภายใน array โดยการนำสมาชิกใหม่ไปต่อท้าย
+//characters.push('K')
+//console.log(characters)
+
+
